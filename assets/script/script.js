5514a5541b229a94c034bd8c5cf85d39edb7ac08
Fernando Borel <fernandomenezesborel@gmail.com>
2021-10-13 20:57:25 -0300

    Relógio analógico em fase final

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,8 +11,13 @@ function updateClock() {
 
     digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 
-    sElement.style.transform = 'rotate(45deg)';
-
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+    
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
 }
 
 function fixZero(time) {
@@ -24,3 +29,4 @@ function fixZero(time) {
 }
 
 setInterval(updateClock, 1000);
+updateClock();
